351dae193338ec2fe357c9e7dc3948905d0b8386
Sadnan Islam <sadnan15916q@gmail.com>
2023-09-09 04:15:57 +0600

    create geocoord file and add basic framework

---
+++ GeoCoord.py
@@ -0,0 +1,28 @@
+from random import randint
+
+class Point :
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rectangle(Point) :
+    def __init__(self):
+        self.downLeft = (randint(0,9), randint(0,9))
+        self.upperRight = (randint(10,19), randint(10,19))
+    
+    def point_in_rectangle(self, point):
+        if self.downLeft[0] < point.x < self.upperRight[0] and self.downLeft[1] < point.y < self.upperRight[1]:
+            return True
+
+random_rect = Rectangle()
+
+print(f"The rectangles borders are: {random_rect.downLeft} & {random_rect.upperRight}")
+
+user_x = input("Enter your value for 'X': ")
+user_y = input("Enter your value for 'Y': ")
+user_point = Point(float(user_x), float(user_y))
+
+if random_rect.point_in_rectangle(user_point):
+    print("Great! You are correct.")
+else:
+    print("Sorry, try again.")
