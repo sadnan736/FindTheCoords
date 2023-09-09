5d6d47516d55dc5df8e749e0180f8a865d5f9ee5
Sadnan Islam <sadnan15916q@gmail.com>
2023-09-09 18:21:53 +0600

    change rectangle corener coordinates limit and add try except for value error the upper limit for the cordinataes of the rectangle was sys.maxlimit changed it to a more user-friendly one (20 and 40) changed te user input to encorporate Value Errors.

--- GeoCoord.py
+++ GeoCoord.py
@@ -8,8 +8,8 @@ class Point :
 
 class Rectangle(Point) :
     def __init__(self):
-        self.downLeft = (randint(0, sys.maxsize), randint(0, sys.maxsize))
-        self.upperRight = (randint(self.downLeft[0]+1, sys.maxsize), randint(self.downLeft[1]+1, sys.maxsize))
+        self.downLeft = (randint(0, 20), randint(0, 20))
+        self.upperRight = (randint(self.downLeft[0]+5, 40), randint(self.downLeft[1]+5, 40))
     
     def coord_in_rectangle(self, point):
         if self.downLeft[0] < point.x < self.upperRight[0] and self.downLeft[1] < point.y < self.upperRight[1]:
@@ -19,9 +19,17 @@ random_rect = Rectangle()
 
 print(f"The rectangles corners are: {random_rect.downLeft} & {random_rect.upperRight}")
 
-user_x = input("Enter your value for 'X': ")
-user_y = input("Enter your value for 'Y': ")
-user_point = Point(float(user_x), float(user_y))
+try:
+    user_x = float(input("Enter your value for 'X': "))
+except ValueError:
+    user_x = float(input("Please enter a DIGIT for 'X': "))
+
+try:
+    user_y = float(input("Enter your value for 'X': "))
+except ValueError:
+    user_y = float(input("Please enter a DIGIT for 'X': "))
+
+user_point = Point(user_x, user_y)
 
 if random_rect.coord_in_rectangle(user_point):
     print("Great! You are correct.")
